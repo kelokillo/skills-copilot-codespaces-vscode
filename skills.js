df531411e68b81646336e4d87f0074eba39d0419
Kalfukoyam Treuquil Calfumil <kalfukoyam@gmail.com>
2023-12-01 03:44:18 +0000

    Copilot first commit

---
+++ skills.js
@@ -0,0 +1,9 @@
+function calculateNumbers(var1, var2) {
+    const sum = var1 + var2;
+    const difference = var1 - var2;
+    const product = var1 * var2;
+    const quotient = var1 / var2;
+    const remainder = var1 % var2;
+  
+    return [sum, difference, product, quotient, remainder];
+  }
